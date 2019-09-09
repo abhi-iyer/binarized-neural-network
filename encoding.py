924114e47fe6303ae672e70ced9e7aadfbfc29f4
abhi-iyer <abhi.iyer.ai@gmail.com>
2019-09-09 16:03:27 -0700

    lower bound for encoding established

--- encoding.py
+++ encoding.py
@@ -12,9 +12,40 @@ def decompose_net(net):
             mean = torch.cuda.FloatTensor(module.running_mean).unsqueeze(dim=1)
             std = torch.cuda.FloatTensor(module.running_var ** 0.5).unsqueeze(dim=1)
 
-            bn.append((scale, bias, mean, std))
+            bn.append({'scale' : scale,
+                       'bias' : bias,
+                       'mean' : mean,
+                       'std' : std})
 
         if 'fc' in name:
-            linear.append(module)
+            linear.append({'weight' : module.weight,
+                           'bias' : module.bias})
             
     return {"linear" : linear, "bn" : bn}
+
+
+def lower_bound():
+    decomp = decompose_net(bnn.net)
+    linear, bn = decomp['linear'], decomp['bn']
+    
+    C = [[] for _ in range(len(bn))]
+    D = [[] for _ in range(len(bn))]
+
+    for i in range(len(bn)):
+        for j in range(len(bn[i]['scale'])):
+            val = ((-bn[i]['std'][j]/bn[i]['scale'][j])*bn[i]['bias'][j] + \
+                   bn[i]['mean'][j] - linear[i]['bias'][j]).item()
+
+            if val > 0:
+                val = ceil(val)
+            else:
+                val = floor(val)
+
+            C[i].append(val)
+
+    for i in range(len(bn)):
+        for j in range(len(linear[i]['weight'])):
+            C_prime = ceil(C[i][j]/2 + linear[i]['weight'][j, :].sum().item()/2)
+            val = C_prime + (linear[i]['weight'][j, :] == -1).sum().item()
+
+            D[i].append(val)
--- models.py
+++ models.py
@@ -20,25 +20,17 @@ class MNIST_BNN(nn.Module):
         self.fc1 = BinarizeLinear(784, 784*2)
 #         self.htanh1 = nn.Hardtanh()
         self.bn1 = nn.BatchNorm1d(784*2, eps=0)
-#         init.normal_(self.bn1.weight.data, mean=1, std=0.02)
-#         init.constant_(self.bn1.bias.data, 0)
         
         self.fc2 = BinarizeLinear(784*2, 784*2)
 #         self.htanh2 = nn.Hardtanh()
         self.bn2 = nn.BatchNorm1d(784*2, eps=0)
-#         init.normal_(self.bn2.weight.data, mean=1, std=0.02)
-#         init.constant_(self.bn2.bias.data, 0)
         
         self.fc3 = BinarizeLinear(784*2, 784*2)
 #         self.htanh3 = nn.Hardtanh()
         self.bn3 = nn.BatchNorm1d(784*2, eps=0)
-#         init.normal_(self.bn3.weight.data, mean=1, std=0.02)
-#         init.constant_(self.bn3.bias.data, 0)
         
         self.fc4 = BinarizeLinear(784*2, 10)
-        
-#         self.logsoftmax = nn.LogSoftmax(dim=1)
-            
+                    
     def forward(self, x):                        
         x = x.view(-1, 28*28)
         x = self.fc1(x)
@@ -56,7 +48,4 @@ class MNIST_BNN(nn.Module):
         x = self.fc4(x)
         
         return x
-                
-#         return self.logsoftmax(x)
-#         return self.softmax(x)
         
