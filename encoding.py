52f8ac2d2e71f5dd5d0217ce26e2f4761bf233bb
abhi-iyer <abhi.iyer.ai@gmail.com>
2019-09-09 18:57:13 -0700

    more cleaning;

--- encoding.py
+++ encoding.py
@@ -1,5 +1,17 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+from torch.autograd import Function
+import numpy as np
+import os
+from torchvision import datasets as datasets
+from torchvision import transforms as transforms
+from torch.utils.data import DataLoader as DataLoader
+from math import ceil, floor
+
 
 def decompose_net(net):
     linear = []
@@ -24,8 +36,8 @@ def decompose_net(net):
     return {"linear" : linear, "bn" : bn}
 
 
-def lower_bound():
-    decomp = decompose_net(bnn.net)
+def lower_bound(net):
+    decomp = decompose_net(net)
     linear, bn = decomp['linear'], decomp['bn']
     
     C = [[] for _ in range(len(bn))]
@@ -49,3 +61,5 @@ def lower_bound():
             val = C_prime + (linear[i]['weight'][j, :] == -1).sum().item()
 
             D[i].append(val)
+            
+    return D
